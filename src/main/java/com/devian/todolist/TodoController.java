08081ff2e7607779863ab7b70ba5248fe61a0a19
Mark Ian Ebuen <ianebuen2001@gmail.com>
2025-08-18 03:03:59 +0800

    Configure the application properties

--- src/main/java/com/devian/todolist/TodoController.java
+++ src/main/java/com/devian/todolist/TodoController.java
@@ -1,4 +1,4 @@
-package com.devian.todolist.controller;
+package com.devian.todolist;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
